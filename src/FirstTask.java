66bb34d8ec78f6a521f64123a8ee124a522c20c8
m0narc4 <alexeycok1331@gmail.com>
2023-03-03 11:16:36 +0300

    master1

---
+++ src/FirstTask.java
@@ -0,0 +1,8 @@
+public class FirstTask {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0]);
+        } else
+            System.out.println("There is no arguments!");
+    }
+}
